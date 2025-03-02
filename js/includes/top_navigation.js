09245528c5278a776dbcb2fc5d464d8ddc0ffcb5
rhickwelfloresvaldez <rhickwelfloresvaldez@gmail.com>
2025-03-02 17:45:33 +0800

    Updated top navigation

---
+++ js/includes/top_navigation.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const basePath = window.location.pathname.includes("/doers/") ? "/doers/" : "/";
+
+    fetch(basePath + "includes/includes.json")
+        .then(response => response.json())
+        .then(data => {
+            if (data.topNavigation) {
+                return fetch(basePath + data.topNavigation);
+            } else {
+                throw new Error("Navigation path not found in JSON.");
+            }
+        })
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("top-navigation").innerHTML = html;
+        })
+        .catch(error => console.error("Error loading navigation:", error));
+});
+
+function showMobileNav() {
+    const mobileNav = document.querySelector('.mobile-nav');
+    mobileNav.style.display = 'flex';
+    mobileNav.classList.add('slide-in-top');
+    document.body.classList.add('no-scroll');
+}
+
+function hideMobileNav() {
+    const mobileNav = document.querySelector('.mobile-nav');
+    mobileNav.classList.add('slide-out-top');
+
+    mobileNav.addEventListener('animationend', function () {
+        mobileNav.style.display = 'none';
+        mobileNav.classList.remove('slide-out-top');
+        document.body.classList.remove('no-scroll');
+    }, { once: true });
+}
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+    // console.log(document.getElementById("logo"));
+    // const basePath = window.location.pathname.includes("/doers/") ? "/doers/" : "/";
+
+    // fetch(basePath + "assets/assets.json")
+    //     .then(response => response.json())
+    //     .then(assets => {
+    //         const logoImg = document.getElementById("logo");
+    //         if (logoImg && assets["logoWithText"]) {
+    //             logoImg.src = basePath + assets["logoWithText"];
+    //             console.log(wo)
+    //         } else {
+    //             console.error("Logo not found in JSON.");
+    //         }
+    //     })
+    //     .catch(error => console.error("Error loading assets:", error));
+// });
